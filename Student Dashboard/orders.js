947646bfe134205e654036d83bcaf675eb0c337d
Jasurbek Khamroev <95095145+LastCoderBoy@users.noreply.github.com>
2024-11-12 16:21:05 +0100

    UI view only frontend

---
+++ Student Dashboard/orders.js
@@ -0,0 +1,17 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        status: 'In Progress'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        status: 'Failed'
+    },
+    {
+        productName: 'Java Tutorial',
+        productNumber: '36452',
+        status: 'Passed'
+    },
+]
